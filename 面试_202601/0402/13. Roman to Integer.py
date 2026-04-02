d6e888eb0a512aa348c1af45901d2c45c47fd8be
jie Wu <wjkaola123@163.com>
2026-04-02 17:37:20 +0800

    add

--- 面试_202601/0402/13. Roman to Integer.py
+++ 面试_202601/0402/13. Roman to Integer.py
@@ -1,3 +1,53 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        start = 0
+        end = start + 1
+        n = len(s)
+        ans = 0
+        while end < n:
+            if s[start] == 'I' and s[end] in ['V', 'X']:
+                if s[end] == 'V':
+                    ans += 4
+                elif s[end] == 'X':
+                    ans += 9
+                start += 2
+                end += 2
+            elif s[start] == 'X' and s[end] in ['L', 'C']:
+                if s[end] == 'L':
+                    ans += 40
+                elif s[end] == 'C':
+                    ans += 90
+                start += 2
+                end += 2
+            elif s[start] == 'C' and s[end] in ['D', 'M']:
+                if s[end] == 'D':
+                    ans += 400
+                elif s[end] == 'M':
+                    ans += 900
+                start += 2
+                end += 2
+            else:
+                ans += d[s[start]]
+                start += 1
+                end += 1
+
+        if start == n - 1:
+            ans += d[s[start]]
+
+        return ans
+
+
+s = Solution()
+assert s.romanToInt('III') == 3
+assert s.romanToInt('LVIII') == 58
+assert s.romanToInt('MCMXCIV') == 1994
