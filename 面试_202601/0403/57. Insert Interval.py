ab33d4b83215936bfb8b73cc65ea5ec860519901
jie Wu <wjkaola123@163.com>
2026-04-03 17:28:44 +0800

    add

--- 面试_202601/0403/57. Insert Interval.py
+++ 面试_202601/0403/57. Insert Interval.py
@@ -10,17 +10,17 @@ class Solution:
             ans.append(merged_interval)
             return ans
 
-        flag = 0
+        flag = 0  # 初始态
         for i in range(n):
             # 判断当前区间是否与新区间重叠
             if merged_interval[0] > intervals[i][1] or merged_interval[1] < intervals[i][0]:  # 不重叠
                 if flag == 1:
                     ans.append(merged_interval)
-                    flag = 2
+                    flag = 2  # 已合并
                 ans.append(intervals[i])
             else:
                 # 重叠,合并两个区间
-                flag = 1
+                flag = 1  # 合并中
                 left = min(intervals[i][0], merged_interval[0])
                 right = max(intervals[i][1], merged_interval[1])
                 merged_interval = [left, right]
