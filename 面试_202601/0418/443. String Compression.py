aff44338aee9041979eb544b227fbe195e703333
jie Wu <wjkaola123@163.com>
2026-04-18 16:03:37 +0800

    add

--- 面试_202601/0418/443. String Compression.py
+++ 面试_202601/0418/443. String Compression.py
@@ -7,40 +7,52 @@ class Solution:
         if not chars:
             return 0
 
+        n = len(chars)
+        start = end = 0
         k = 0
-        od = OrderedDict()
-        for c in chars:
-            if c not in od:
-                od[c] = 1
-            else:
-                od[c] += 1
+        while end < n:
+            while end < n and chars[start] == chars[end]:
+                end += 1
 
-        for key, val in od.items():
-            if val == 1:
-                chars[k] = key
+            length = end - start
+            if length == 1:
+                chars[k] = chars[start]
                 k += 1
-            elif val >= 2 and val <= 9:
-                chars[k] = key
-                chars[k + 1] = f'{val}'
+            elif length >= 2 and length <= 9:
+                chars[k] = chars[start]
+                chars[k + 1] = f'{length}'
                 k += 2
             else:
                 # 10 or longer
-                value_strings = str(val)
-                n = len(value_strings)
-                chars[k] = key
+                value_strings = str(length)
+                num_length = len(value_strings)
+                chars[k] = chars[start]
                 k += 1
-                for i in range(n):
+                for i in range(num_length):
                     chars[k] = value_strings[i]
                     k += 1
-
+            start = end
         return k
 
 
 s = Solution()
+# test case 1
 chars = ["a", "a", "b", "b", "c", "c", "c"]
 k = s.compress(chars)
 assert k == 6
 assert "".join(chars[:6]) == 'a2b2c3'
 
+# test case 2
 k = s.compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"])
 assert k == 4
+
+# test case 3
+chars = ["a", "b", "c"]
+s.compress(chars)
+print(chars)
+
+# test case 4
+chars = ["a", "a", "a", "a", "a", "a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b",
+         "b", "b", "b", "b", "b", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c"]
+s.compress(chars)
+print(chars)
