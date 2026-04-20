e9464706e89e8b9166a1ab604e22904a0d2f580f
jie Wu <wjkaola123@163.com>
2026-04-20 11:51:24 +0800

    add

---
+++ 面试_202601/0419/735. Asteroid Collision.py
@@ -0,0 +1,46 @@
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for ast in asteroids:
+            if not stack:
+                stack.append(ast)
+                continue
+            else:
+                stop_compare = False
+                while not stop_compare:
+                    if not stack:
+                        break
+
+                    # 比较当前元素和栈顶元素
+                    if stack[-1] * ast > 0:  # 符号相同
+                        stack.append(ast)
+                        stop_compare = True
+                    else:
+                        # 栈顶元素符号和当前元素不同, 分成两种情况:
+                        # 1. 栈顶为负数,当前数为正,则不会相撞
+                        # 2. 栈顶为正数,当前数为负,则会相撞
+                        if stack[-1] < 0 and ast > 0:
+                            stack.append(ast)
+                            stop_compare = True
+                        elif stack[-1] > 0 and ast < 0:
+                            if abs(stack[-1]) > abs(ast):
+                                stop_compare = True
+                            elif abs(stack[-1]) == abs(ast):
+                                stack.pop()
+                                stop_compare = True
+                            elif abs(stack[-1]) < abs(ast):
+                                stack.pop()
+                                stop_compare = False
+                                if not stack:
+                                    stack.append(ast)
+                                    stop_compare = True
+
+        return stack
+
+
+asteroids = [3, 5, -6, 2, -1, 4]
+s = Solution()
+assert s.asteroidCollision(asteroids) == [-6, 2, 4]
