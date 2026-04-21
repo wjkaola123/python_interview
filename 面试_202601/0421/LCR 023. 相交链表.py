0715b9d7c9bc7e15fa644cb314797461311a938c
jie Wu <wjkaola123@163.com>
2026-04-21 22:49:09 +0800

    add

---
+++ 面试_202601/0421/LCR 023. 相交链表.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        p = headA
+        q = headB
+
+        while p is not q:
+            p = p.next if p else headB
+            q = q.next if q else headA
+
+        return p
