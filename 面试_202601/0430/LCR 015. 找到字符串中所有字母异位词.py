67dea90c6280bf5dfe552d266d287d38adbfc9b2
jie Wu <wjkaola123@163.com>
2026-04-30 18:19:04 +0800

    add

--- 面试_202601/0430/LCR 015. 找到字符串中所有字母异位词.py
+++ 面试_202601/0430/LCR 015. 找到字符串中所有字母异位词.py
@@ -1,23 +1,31 @@
-from typing import List
+from typing import List, Counter
 
 
 class Solution:
 
-    def is_anagrams(self, s: str, p: str):
-        if len(s) != len(p):
+    def is_same_dict(self, d1: dict, d2: dict) -> bool:
+        if len(d1) != len(d2):
             return False
 
-        if sorted(s) != sorted(p):
-            return False
+        for key, val in d1.items():
+            if key not in d2:
+                return False
+
+            if d2[key] != val:
+                return False
 
         return True
 
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n = len(s)
         m = len(p)
+        c1 = Counter(p)
+        d1 = dict(c1.most_common())
         ans = []
         for i in range(n):
-            if self.is_anagrams(s[i: i + m], p):
+            c1 = Counter(s[i: i + m])
+            d2 = dict(c1.most_common())
+            if self.is_same_dict(d1, d2):
                 ans.append(i)
 
         return ans
