1098fa5b2ae6161c6d4c24ae342a70e492ee00d5
jie Wu <wjkaola123@163.com>
2026-05-07 23:16:39 +0800

    add

---
+++ 面试_202601/0507/LCR 033. 字母异位词分组.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            key = "".join(sorted(s))
+            if key not in d:
+                d[key] = [s]
+            else:
+                d[key].append(s)
+
+        return list(d.values())
+
+
+s = Solution()
+print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
