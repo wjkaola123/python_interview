5b8dd669fc8423dd31751f278d6c372dd2b93870
jie Wu <wjkaola123@163.com>
2026-05-24 12:02:30 +0800

    add

--- 面试_202601/iterator.py
+++ 面试_202601/iterator.py
@@ -26,3 +26,27 @@ print(isinstance(bs, Iterable))
 
 for b in bs:
     print(b)
+
+print("=" * 30)
+
+
+class BookShelf2(Iterable):
+
+    def __init__(self):
+        self.books = ['西游记', '红楼梦', '三国演义', '水浒传']
+        self.current_index = 0
+
+    def __iter__(self):
+        while True:
+            if 0 <= self.current_index < len(self.books):
+                yield self.books[self.current_index]
+                self.current_index += 1
+            else:
+                break
+
+
+bs2 = BookShelf2()
+print(isinstance(bs2, Iterator))
+print(isinstance(bs2, Iterable))
+for b in bs2:
+    print(b)
